2a09989354b48bbe2cd3174862dcea3c6ca431a9
Kobzpik <prabath.pik2299@gmail.com>
2021-10-30 07:49:54 +0530

    intial commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+//fix nav bar
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > 300) {
+            $(".test").addClass("fixn");
+        } else {
+            $(".test").removeClass("fixn");
+        }
+    })
+});
+
+
+
+//contact form validation
+function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    if (name.length < 3) {
+        alert("Please enter valid name");
+        return false;
+    }
+    if (subject.length < 2) {
+        alert("Please enter valid subject");
+        return false;
+    }
+    if (email.indexOf("@") == -1 || email.length < 6) {
+        alert("Please enter valid E-mail");
+        return false;
+    }
+
+    if (message.length <= 3) {
+        alert("Please enter valid message");
+        return false;
+    } else {
+        alert("Form Submitted Successfully!");
+        return true;
+    }
+
+};
+
+
+//Testimonials  Owl Carousel
+$(document).ready(function() {
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        margin: 5,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            900: {
+                items: 3
+            }
+        }
+    })
+});
